e737f93b6163a24b596f3adb794303cd54b9e6a7
Belfor Acuña <131080733+belfor-acuna@users.noreply.github.com>
2025-02-12 16:22:13 -0300

    ci(CI/CD Playground): :rocket: Update cors for testing from local frontend

--- expressPlayground/index.js
+++ expressPlayground/index.js
@@ -3,10 +3,18 @@ import {PORT} from "./config/environment.js";
 
 import nodemailerRoutes from "./routes/nodemailerRoutes.js";
 import testRoutes from "./routes/testRoutes.js";
-
+import cors from 'cors';
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+const corsOptions = {
+  origin: "https://belfor-acuna-portfolio.onrender.com", 
+  methods: ["GET", "POST"], 
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOptions)); // Usar CORS con la configuración personalizada
+app.use(cors());
 app.use("/mail", nodemailerRoutes);
 app.use("/testexpress", testRoutes);
 app.listen(PORT, () => {
